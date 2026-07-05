079aebdbc8e7e620089b3eb855b88f2358cee00f
Vismaya6 <vismayatheresabenny2028@cs.sjcetpalai.ac.in>
2026-07-05 15:26:16 +0530

    Updated YouTube clone UI and API fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,18 @@
 import axios from "axios";
 
-const BASE_URL = "https://youtube-v31.p.rapidapi.com";
-
-const options = {
-  params: {
-    maxResults: "20",
-  },
-  headers: {
-    "X-RapidAPI-Key": "bd3c96af35msh3668d31ce78e5c1p12f11ajsndf2290da27ba",
-    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
-  },
-};
+const BASE_URL = "https://www.googleapis.com/youtube/v3";
+const API_KEY = "AIzaSyC7cqz2dphH0M9HPdBUqoiXcSCl31mIFnU"; 
 
 export const fetchFromAPI = async (url) => {
   try {
     const response = await axios.get(
       `${BASE_URL}/${url}`,
-      options
+      {
+        params: {
+          key: API_KEY,
+          maxResults: 20,
+        },
+      }
     );
 
     return response.data;
